e242419a36c796d59b3c005788705e9c7ea87bdf
megagros687 <megagros687@SDS-L-19QLG63>
2025-02-07 14:10:10 -0500

    Add project files.

---
+++ StartGame/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace StartGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(279, 252);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(244, 72);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.countdownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.countdownLabel.Location = new System.Drawing.Point(250, 83);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(287, 84);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = "The Game";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countdownLabel.Click += new System.EventHandler(this.countdownLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "The Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ StartGame/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace StartGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //countdown
+            countdownLabel.Text = "3";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            countdownLabel.Text = "2";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            countdownLabel.Text = "1";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.airhorn);
+            countdownLabel.Text = "GO!!";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            startButton.ForeColor = Color.Black;
+            countdownLabel.ForeColor = Color.Black;
+        }
+
+        private void countdownLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
